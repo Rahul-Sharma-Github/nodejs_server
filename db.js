278d309f912336955a96a42d0d515b101237c26e
Rahul <rahulsharma3258@gmail.com>
2024-04-02 14:12:15 +0530

    Removed deprecated option "useNewUrlParser" from connect function |

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/my_database', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/my_database', {});
 
-
+// write comments here .....
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
