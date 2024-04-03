af57b1106c98520fab4947e50083b12a3adb3ebd
Rahul <rahulsharma3258@gmail.com>
2024-04-03 14:35:37 +0530

    handled error while initially connecting to MongoDB local database |

--- db.js
+++ db.js
@@ -3,13 +3,23 @@ const mongoose = require('mongoose');
 
 
 // Establishing connection with MongoDB Local Database through connection String
-mongoose.connect('mongodb://localhost:27017/jumbo_foods_database', {});
+try {
+  await mongoose.connect('mongodb://localhost:27017/jumbo_foods_database', {});
+} catch (error) {
+  // handling error while connecting to database
+  handleError(error);
+}
+
 
 // Getting connection instance into db variable
 const db = mongoose.connection;
 
+// handling error after initial established connection
 // if error occured, then it shows error in Console
 db.on('error', console.error.bind(console, 'Database connection error !:'));
+
+
+
 
 // When connection is successfully established, it shows success message in console
 db.once('open', function() {
